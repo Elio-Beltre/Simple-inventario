05f4897ea0332e7ae51cdb07b02ee2dd6affe89c
Laboratorio 109 Pc-15 <lab-109-inf-15>
2023-09-28 19:02:43 -0400

    Mejoras aplicadas de la version v2

--- src/SimpleInventario.java
+++ src/SimpleInventario.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class SimpleInventario {
     public static void agregarArticulo(String a){
 
@@ -10,9 +12,22 @@ public class SimpleInventario {
 
     }
     public static void imprimirInventario(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ingrese su nombre: ");
+        String usuario = scanner.nextLine();
+        System.out.println("Bienvenid@ "+ usuario);
+        String articuloExistente = obtenerNombreArticulo();
+        System.out.println("Articulo existente: "+articuloExistente);
 
     }
+    public  static String obtenerNombreArticulo(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el nombre del articulo: ");
+        String nombre = scanner.nextLine();
+        System.out.println("Usted ha ingresado el articulo: "+ nombre );
+        return nombre;
+    }
     public static void main(String[] args) {
-        System.out.println(" Ingrese una opcion ");
+        imprimirInventario();
     }
 }
